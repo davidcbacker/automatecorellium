ac4b17054970a87c6936c0a45d26774d9abc30aa
davidcbacker <20198171+davidcbacker@users.noreply.github.com>
2025-06-06 14:19:36 -0400

    Create bash_functions.sh

---
+++ src/bash_functions.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+#
+# Define reusable functions for CI
+
+start_instance() {
+  local instance_id="$1"
+  echo "Starting instance ${instance_id}"
+  corellium instance start "${instance_id}" --wait || true
+}
+
+wait_for_agent_ready() {
+  local instance_id="$1"
+  local sleep_time=10
+  local project_id
+  project_id=$(corellium instance get --instance "${instance_id}" | jq -r '.project')
+  local ready_status
+  ready_status=$(corellium ready --instance "${instance_id}" --project "${project_id}" 2>/dev/null | jq -r '.ready')
+
+  while [ "${ready_status}" != 'true' ]; do
+    echo "Agent is not ready yet. Checking again in ${sleep_time} seconds."
+    sleep "${sleep_time}"
+    ready_status=$(corellium ready --instance "${instance_id}" --project "${project_id}" | jq -r '.ready')
+  done
+}
+
+install_corellium_cafe() {
+  local instance_id="$1"
+  local project_id
+  project_id=$(corellium instance get --instance "${instance_id}" | jq -r '.project')
+  local ipa_url="https://www.corellium.com/hubfs/Corellium_Cafe.ipa"
+  local ipa_filename
+  ipa_filename=$(basename "${ipa_url}")
+
+  echo "Downloading ${ipa_filename}"
+  wget --no-verbose "${ipa_url}"
+  echo "Installing ${ipa_filename}"
+  corellium apps install --instance "${instance_id}" --project "${project_id}" --app "${ipa_filename}"
+
+  if [ "$?" -gt 0 ]; then
+    echo "Error installing app" >&2
+    exit 1
+  fi
+  echo "Successfully installed ${ipa_filename}"
+}
+
+run_matrix() {
+  local instance_id="$1"
+  echo "Creating MATRIX assessment"
+  local assessment_id
+  assessment_id=$(corellium matrix create-assessment --instance "${instance_id}" --bundle com.corellium.Cafe | jq -r '.id')
+
+  if [ -z "${assessment_id}" ]; then
+    echo "Failed to create assessment" >&2
+    exit 1
+  fi
+  echo "Created MATRIX assessment ${assessment_id}"
+
+  echo "Starting MATRIX monitoring"
+  corellium matrix start-monitor --instance "${instance_id}" --assessment "${assessment_id}"
+
+  echo "Waiting for monitoring to start"
+  local assessment_status
+  assessment_status=$(corellium matrix get-assessment --instance "${instance_id}" --assessment "${assessment_id}" | jq -r '.status')
+
+  while [ "${assessment_status}" != 'monitoring' ]; do
+    echo "Current assessment status is ${assessment_status}"
+    sleep 5
+    assessment_status=$(corellium matrix get-assessment --instance "${instance_id}" --assessment "${assessment_id}" | jq -r '.status')
+  done
+
+  echo "Stopping MATRIX monitoring"
+  corellium matrix stop-monitor --instance "${instance_id}" --assessment "${assessment_id}"
+
+  echo "Waiting for monitoring to stop"
+  assessment_status=$(corellium matrix get-assessment --instance "${instance_id}" --assessment "${assessment_id}" | jq -r '.status')
+
+  while [ "${assessment_status}" != 'readyForTesting' ]; do
+    echo "Current assessment status is ${assessment_status}"
+    sleep 5
+    assessment_status=$(corellium matrix get-assessment --instance "${instance_id}" --assessment "${assessment_id}" | jq -r '.status')
+  done
+
+  echo "Running MATRIX test"
+  corellium matrix test --instance "${instance_id}" --assessment "${assessment_id}"
+
+  echo "Waiting for test to complete"
+  assessment_status=$(corellium matrix get-assessment --instance "${instance_id}" --assessment "${assessment_id}" | jq -r '.status')
+
+  while [ "${assessment_status}" != 'complete' ]; do
+    echo "Current assessment status is ${assessment_status}"
+    sleep 60
+    assessment_status=$(corellium matrix get-assessment --instance "${instance_id}" --assessment "${assessment_id}" | jq -r '.status')
+  done
+
+  local report_id
+  report_id=$(corellium matrix get-assessment --instance "${instance_id}" --assessment "${assessment_id}" | jq -r '.reportId')
+
+  echo "Downloading MATRIX report ${report_id} as HTML"
+  corellium matrix download-report --instance "${instance_id}" --assessment "${assessment_id}" > "matrix_report_${report_id}.html"
+
+  echo "Downloading MATRIX report ${report_id} as JSON"
+  corellium matrix download-report --instance "${instance_id}" --assessment "${assessment_id}" --format json > "matrix_report_${report_id}.json"
+}

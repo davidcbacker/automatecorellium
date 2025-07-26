40a2ead9fa92facc64e3d6623477f209a5d5f6e8
davidcbacker <20198171+davidcbacker@users.noreply.github.com>
2025-07-26 13:25:24 -0400

    Update functions.sh

--- src/functions.sh
+++ src/functions.sh
@@ -71,7 +71,7 @@ get_instance_status()
   corellium instance get --instance "${instance_id}" | jq -r '.state'
 }
 
-get_instance_service_ip()
+get_instance_services_ip()
 {
   local instance_id="$1"
   corellium instance get --instance "${instance_id}" | jq -r '.serviceIp'
@@ -405,4 +405,20 @@ install_usbfluxd_and_dependencies()
 
   cd "${HOME}/" || exit 1
   rm -rf "${temp_compile_dir:?}/"
+}
+
+run_usbfluxd_and_dependencies()
+{
+  sudo systemctl start usbmuxd
+  sudo systemctl status usbmuxd
+  sudo avahi-daemon &
+  sudo usbfluxd -f -n &
+}
+
+add_instance_to_usbfluxd()
+{
+  local INSTANCE_ID="$1"
+  local INSTANCE_SERVICES_IP="$(get_instance_services_ip "${INSTANCE_ID}")"
+  local USBFLUXD_PORT='5000'
+  usbfluxctl add "${INSTANCE_SERVICES_IP}:${USBFLUXD_PORT}"
 }

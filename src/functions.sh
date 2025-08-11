8b9c4dacff06009818d8d3906c0a572dfa303265
davidcbacker <20198171+davidcbacker@users.noreply.github.com>
2025-08-11 11:39:51 -0400

    refactor: use single instance get not list

--- src/functions.sh
+++ src/functions.sh
@@ -33,8 +33,8 @@ log_stdout()
 ensure_instance_exists()
 {
   local INSTANCE_ID="$1"
-  if ! corellium list |
-    jq --exit-status --arg id "$INSTANCE_ID" '.[] | select(.id == $id)' \
+  if ! corellium instance get --instance "$INSTANCE_ID" |
+    jq -e --arg id "${INSTANCE_ID}1" 'select(.id == $id)' \
     > /dev/null; then
     echo "Error, instance ${INSTANCE_ID} does not exist." >&2
     exit 1

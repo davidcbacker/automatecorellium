50889859035dbb962ab653f9dd621ea5caa88734
davidcbacker <20198171+davidcbacker@users.noreply.github.com>
2025-11-05 13:21:24 -0500

    Update appium_interactions_template.py

--- src/util/appium_interactions_template.py
+++ src/util/appium_interactions_template.py
@@ -1,40 +1,45 @@
+"""
+Automate Corellium virtual device interactions using Appium.
+"""
+
+from time import sleep
+import sys
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 from appium.webdriver.common.appiumby import AppiumBy
-from time import sleep
-from sys import argv
 
 # ==== CONSTANTS: CORELLIUM DEVICE ====
-default_services_ip = '10.11.1.1'
-default_adb_port = '5001'
+DEFAULT_SERVICES_IP = '10.11.1.1'
+DEFAULT_ADB_PORT = '5001'
 
 # ==== CONSTANTS: TARGET APP ====
-target_app_package = 'com.yourpackage.name'
-target_app_activity = '.MainActivity'
+TARGET_APP_PACKAGE = 'com.mypackage.name'
+TARGET_APP_ACTIVITY = '.MainActivity'
 
 # ==== CONSTANTS: APPIUM SERVER ====
-appium_server_ip = '127.0.0.1'
-appium_server_port = '4723'
-appium_server_url = f'http://{appium_server_ip}:{appium_server_port}'
+APPIUM_SERVER_IP = '127.0.0.1'
+APPIUM_SERVER_PORT = '4723'
+APPIUM_SERVER_SOCKET = f'http://{APPIUM_SERVER_IP}:{APPIUM_SERVER_PORT}'
 
-def run_cafe_app_test():
+def run_app_automation():
+    '''Launch the app and interact using Appium commands.'''
     options = UiAutomator2Options()
     options.set_capability('platformName', 'Android')
     options.set_capability('appium:automationName', 'UiAutomator2')
-    options.set_capability('appium:udid', corellium_device_appium_udid)
-    options.set_capability('appium:appPackage', target_app_package)
-    options.set_capability('appium:appActivity', target_app_activity)
+    options.set_capability('appium:udid', CORELLIUM_DEVICE_APPIUM_UDID)
+    options.set_capability('appium:appPackage', TARGET_APP_PACKAGE)
+    options.set_capability('appium:appActivity', TARGET_APP_ACTIVITY)
     options.set_capability('appium:noReset', False) # set to true for MATRIX runs
 
     try:
         print("Starting session...")
-        driver = webdriver.Remote(appium_server_url, options=options)
+        driver = webdriver.Remote(APPIUM_SERVER_SOCKET, options=options)
         print("Successfully loaded target app.")
 
         # ==== COPY-PASTE THE EXACT APPIUM INSPECTOR RECORDING SEQUENCE ====
 
         # ==== END OF COPY-PASTE SECTION ====
-        
+
         sleep(2)
         print("All steps executed on Corellium Android device.")
 
@@ -44,20 +49,20 @@ def run_cafe_app_test():
 
     finally:
         print("Terminating app and closing session.")
-        driver.terminate_app('com.corellium.cafe') # remove this line for MATRIX runs
+        driver.terminate_app(TARGET_APP_PACKAGE) # remove this line for MATRIX runs
         driver.quit()
 
 if __name__ == "__main__":
-    match len(argv):
+    match len(sys.argv):
         case 1:
-            corellium_device_appium_udid = f'{default_services_ip}:{default_adb_port}'
-            print(f'Defaulting to Corellium device at {corellium_device_appium_udid}.')
+            CORELLIUM_DEVICE_APPIUM_UDID = f'{DEFAULT_SERVICES_IP}:{DEFAULT_ADB_PORT}'
+            print(f'Defaulting to Corellium device at {DEFAULT_SERVICES_IP}.')
         case 2:
-            target_device_services_ip = argv[1]
-            corellium_device_appium_udid = f'{target_device_services_ip}:{default_adb_port}'
-            print(f'Running cafe app test on device at {corellium_device_appium_udid}.')
+            TARGET_DEVICE_SERVICES_IP = sys.argv[1]
+            CORELLIUM_DEVICE_APPIUM_UDID = f'{TARGET_DEVICE_SERVICES_IP}:{DEFAULT_ADB_PORT}'
+            print(f'Running app test on device at {CORELLIUM_DEVICE_APPIUM_UDID}...')
         case _:
-            print('ERROR: Please provide zero args or pass in the Corellium device services IP.')
-            exit(1)
+            print('ERROR: Please provide zero or pass in the Corellium device services IP.')
+            sys.exit(1)
 
-    run_cafe_app_test()
+    run_app_automation()

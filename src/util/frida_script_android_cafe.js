87c73cef8eed83b9e80cdd90b661e4180ff20b34
davidcbacker <20198171+davidcbacker@users.noreply.github.com>
2025-11-06 11:30:17 -0500

    Create frida_script_android_cafe.js

---
+++ src/util/frida_script_android_cafe.js
@@ -0,0 +1,4 @@
+console.log('------------------------------------');
+console.log('Android Corellium Cafe sample script');
+console.log('Demo of a Frida script running in CI');
+console.log('------------------------------------');

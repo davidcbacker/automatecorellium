2360b66c0b894ecc50af18fffd0845624cdcfcdb
davidcbacker <20198171+davidcbacker@users.noreply.github.com>
2025-11-08 19:41:26 -0500

    Update functions.sh

--- src/functions.sh
+++ src/functions.sh
@@ -263,9 +263,7 @@ is_agent_ready()
   local PROJECT_ID="$2"
   local AGENT_READY_JSON_RESPONSE AGENT_READY_STATUS
   AGENT_READY_JSON_RESPONSE="$(corellium ready --instance "${INSTANCE_ID}" --project "${PROJECT_ID}" 2> /dev/null)" || {
-    echo "DEBUG AGENT_READY_JSON_RESPONSE=${AGENT_READY_JSON_RESPONSE"}
-    log_error 'Failed to get agent ready.'
-    exit 1
+    return 1 # corellium ready exits with nonzero status if agent isn't ready
   }
   AGENT_READY_STATUS="$(echo "${AGENT_READY_JSON_RESPONSE}" | jq -r '.ready')" || {
     log_error 'Failed to parse agent ready JSON response.'

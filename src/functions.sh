edf92706ffed787fc6fd46346910fde2ab8c5049
davidcbacker <20198171+davidcbacker@users.noreply.github.com>
2025-11-25 08:46:59 -0500

    Update functions.sh

--- src/functions.sh
+++ src/functions.sh
@@ -1152,7 +1152,7 @@ EOF
     exit 1
   }
   log_warn 'DEBUG SHOWING THE JSON RESPONSE DETAILS'
-  echo "${OPEN_APPIUM_SESSION_JSON_RESPONSE}"
+  echo "${OPEN_APPIUM_SESSION_JSON_RESPONSE}" >&2
   OPENED_SESSION_ID="$(echo "${OPEN_APPIUM_SESSION_JSON_RESPONSE}" | jq -r '.value.sessionId')" || {
     log_error 'Failed to parse open appium session JSON response.'
     exit 1

2efe907c747196e15897f72767d0dad166998170
davidcbacker <20198171+davidcbacker@users.noreply.github.com>
2025-12-19 09:27:49 -0500

    Update appium_interactions_template.py

--- src/util/appium_interactions_template.py
+++ src/util/appium_interactions_template.py
@@ -23,7 +23,6 @@ DEFAULT_ADB_PORT = '5001'
 
 # ==== TARGET APP ====
 TARGET_APP_PACKAGE = 'com.mypackage.name' # <----- NOTE: Update this for your app's package name
-TARGET_APP_ACTIVITY = '.MainActivity'     # <----- NOTE: Update this for your app based on AndroidManifest.xml
 
 # ==== APPIUM SERVER ====
 APPIUM_SERVER_IP = '127.0.0.1'
@@ -79,7 +78,6 @@ def run_app_automation(udid: str):
     options.set_capability('appium:automationName', 'UiAutomator2')
     options.set_capability('appium:udid', udid)
     options.set_capability('appium:appPackage', TARGET_APP_PACKAGE)
-    options.set_capability('appium:appActivity', TARGET_APP_ACTIVITY)
     options.set_capability('appium:noReset', False) # <----- NOTE: Set this to True for MATRIX assessments
 
     try:

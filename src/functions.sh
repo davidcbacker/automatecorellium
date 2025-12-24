9a213afc35af273e0af3ff89c32cb7e5a9306b4a
davidcbacker <20198171+davidcbacker@users.noreply.github.com>
2025-12-24 08:55:04 -0500

    refactor(scalability): avoid repeated code with get_instance_json (#701)

--- src/functions.sh
+++ src/functions.sh
@@ -295,15 +295,31 @@ soft_stop_instance()
   esac
 }
 
+get_instance_json()
+{
+  local INSTANCE_ID="$1"
+  local GET_INSTANCE_RESPONSE_JSON
+  GET_INSTANCE_RESPONSE_JSON="$(corellium instance get --instance "${INSTANCE_ID}")" || {
+    log_error "Failed to get details for instance ${INSTANCE_ID}. Retrying."
+    GET_INSTANCE_RESPONSE_JSON="$(corellium instance get --instance "${INSTANCE_ID}")" || {
+      log_error "Failed again to get details for instance ${INSTANCE_ID}."
+      exit 1
+    }
+  }
+  echo "${GET_INSTANCE_RESPONSE_JSON}" | jq '.' > /dev/null 2>&1 || {
+    echo "${GET_INSTANCE_RESPONSE_JSON}"
+    log_error 'Failed to parse JSON response.'
+    exit 1
+  }
+  echo "${GET_INSTANCE_RESPONSE_JSON}"
+}
+
 get_instance_status()
 {
   local INSTANCE_ID="$1"
-  local GET_INSTANCE_RESPONSE_JSON INSTANCE_STATE
-  GET_INSTANCE_RESPONSE_JSON="$(corellium instance get --instance "${INSTANCE_ID}")" || {
-    log_error "Failed to get details for instance ${INSTANCE_ID}."
-    return
-  }
-  INSTANCE_STATE="$(echo "${GET_INSTANCE_RESPONSE_JSON}" | jq -r '.state')" || {
+  local GET_INSTANCE_JSON INSTANCE_STATE
+  GET_INSTANCE_JSON="$(get_instance_json "${INSTANCE_ID}")"
+  INSTANCE_STATE="$(echo "${GET_INSTANCE_JSON}" | jq -r '.state')" || {
     log_error "Failed to parse get details JSON response for instance ${INSTANCE_ID}."
     exit 1
   }
@@ -313,12 +329,9 @@ get_instance_status()
 get_instance_services_ip()
 {
   local INSTANCE_ID="$1"
-  local GET_INSTANCE_RESPONSE_JSON INSTANCE_SERVICES_IP
-  GET_INSTANCE_RESPONSE_JSON="$(corellium instance get --instance "${INSTANCE_ID}")" || {
-    log_error "Failed to get details for instance ${INSTANCE_ID}."
-    exit 1
-  }
-  INSTANCE_SERVICES_IP="$(echo "${GET_INSTANCE_RESPONSE_JSON}" | jq -r '.serviceIp')" || {
+  local GET_INSTANCE_JSON INSTANCE_SERVICES_IP
+  GET_INSTANCE_JSON="$(get_instance_json "${INSTANCE_ID}")"
+  INSTANCE_SERVICES_IP="$(echo "${GET_INSTANCE_JSON}" | jq -r '.serviceIp')" || {
     log_error "Failed to parse get details JSON response for instance ${INSTANCE_ID}."
     exit 1
   }
@@ -328,12 +341,9 @@ get_instance_services_ip()
 get_instance_udid()
 {
   local INSTANCE_ID="$1"
-  local GET_INSTANCE_RESPONSE_JSON INSTANCE_UDID
-  GET_INSTANCE_RESPONSE_JSON="$(corellium instance get --instance "${INSTANCE_ID}")" || {
-    log_error "Failed to get details for instance ${INSTANCE_ID}."
-    exit 1
-  }
-  INSTANCE_UDID="$(echo "${GET_INSTANCE_RESPONSE_JSON}" | jq -r '.bootOptions.udid')" || {
+  local GET_INSTANCE_JSON INSTANCE_UDID
+  GET_INSTANCE_JSON="$(get_instance_json "${INSTANCE_ID}")"
+  INSTANCE_UDID="$(echo "${GET_INSTANCE_JSON}" | jq -r '.bootOptions.udid')" || {
     log_error "Failed to parse get details JSON response for instance ${INSTANCE_ID}."
     exit 1
   }

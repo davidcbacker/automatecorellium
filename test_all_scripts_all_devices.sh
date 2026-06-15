d0dac0aacdabc9fcae7494071f68100ebd1cc4c0
davidcbacker <20198171+davidcbacker@users.noreply.github.com>
2026-06-15 15:23:29 -0400

    Update test_all_scripts_all_devices.sh

--- test_all_scripts_all_devices.sh
+++ test_all_scripts_all_devices.sh
@@ -19,16 +19,54 @@ TEST_SCRIPTS=(
 )
 
 ADB_DEVICES=(
-    '10.11.1.2'
-    '10.11.1.4'
-    '10.11.1.8'
-    '10.11.1.10'
+    '10.11.1.3'
+    '10.11.1.5'
+    '10.11.1.7'
+    '10.11.1.9'
+    '10.11.1.11'
+    '10.11.1.13'
+    '10.11.1.15'
+    '10.11.1.17'
 )
 
 cd "${HOME}/Documents/git/automatecorellium/"
+
+echo "Testing all scripts on these rooted ADB-connected devices:"
+for device in "${ADB_DEVICES[@]}"; do  
+    printf '%s ' "'$device'"
+done
+printf '\n'
+
+
+#for device in 10.11.1.17 10.11.1.15 10.11.1.13 10.11.1.11 10.11.1.9 10.11.1.7 10.11.1.5 10.11.1.3; do
+for device in $(seq 3 2 17); do
+  device="10.11.1.$device"
+  adb connect $device:5001
+  adb -s $device:5001 root
+  curl --silent \
+    -X POST "http://127.0.0.1:4723/session" \
+    -H "Content-Type: application/json" \
+    -d "{
+    \"capabilities\": {
+      \"alwaysMatch\": {
+        \"platformName\": \"Android\",
+        \"appium:automationName\": \"UiAutomator2\",
+        \"appium:udid\": \"$device:5001\",
+        \"appium:appPackage\": \"com.corellium.cafe\",
+        \"appium:appActivity\": \".ui.activities.WebViewActivity\",
+        \"appium:uiautomator2ServerInstallTimeout\": 120000,
+        \"appium:uiautomator2ServerLaunchTimeout\": 60000
+      },
+      \"firstMatch\": [{}]
+    }
+  }" | jq -r '.value.sessionId'
+done
+
+
+
 for script in "${TEST_SCRIPTS[@]}"; do
+    echo "Testing $script"
     for device in "${ADB_DEVICES[@]}"; do
-        echo "Testing $script on device: $device"
         python3 "src/util/${script}" "${device}" \
             > "output/test_output_${device}.txt" \
             2> "output/test_stderr_${device}.txt" &
